d9b21e21dfc79e1f9cb985d8adaaa66376a1b64f
George Punter <punter.s.g@gmail.com>
2017-01-28 13:55:25 +0000

    added basic structure for compiler

---
+++ src/compiler/compiler_main.cpp
@@ -0,0 +1,13 @@
+#include "../lexer/lexer_main.cpp"
+
+
+
+int main() {
+  // Lex the input
+  
+  
+  // Take the output from the lexer and parse it
+  
+  // Take the output from the parser and perform necessary functions to compile it
+  
+}
