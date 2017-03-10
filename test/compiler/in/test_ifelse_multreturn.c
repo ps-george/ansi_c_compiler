17c52350835440038db591d430c455eeec0eb9ea
George Punter <punter.s.g@gmail.com>
2017-03-10 16:25:25 +0000

    Renamed test cases

---
+++ test/compiler/in/test_ifelse_multreturn.c
@@ -0,0 +1,20 @@
+int main(){
+  int i = 0;
+  if (i == 5){
+    i = 10;
+  } else {
+    i = 15;
+  }
+  
+  if (i==15){
+    i=48;
+  }
+  else {
+    i=58;
+  }
+  
+  if (i==48){
+    return 37;
+  }
+  return 39;
+}
